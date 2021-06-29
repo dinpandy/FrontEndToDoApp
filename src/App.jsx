6d9e8f335916104e6a7c0b168e94aeff91e3b607
diipan7 <dinesh.pandy@ingka.com>
2021-06-29 13:12:43 +0200

    new section addition

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>extra section</h1>
         <h1>Todo</h1>
       </header>
       <TodoList></TodoList>
