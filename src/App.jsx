51b6c68c4b6af6e542125d22725affb888052033
diipan7 <dinesh.pandy@ingka.com>
2021-06-29 14:46:08 +0200

    setting app.yaml to deploy folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>extra section</h1>
         <h1>Todo</h1>
       </header>
       <TodoList></TodoList>
